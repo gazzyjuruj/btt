b0bdbb59b5dd17e783fc19773ce10aeecd02da4b
Gazzy J. Uruj <ghazanfar.uruj.cool@gmail.com>
2021-07-22 14:50:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ tar -zxvf xmrig-6.13.1-linux-static-x64.tar.gz
 cd xmrig-6.13.1
 POOL=rx.unmineable.com:3333
 WALLET=BTT:TQBff9wvrUvsUCz34NG8ptprhUKwwPMgGK
-WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-Gaz001)
+WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-Gaz)
 
 
-./xmrig -o $POOL -u $WALLET.$WORKEER -tls -k -a rx/0
+./xmrig -o $POOL -u $WALLET.$WORKEER -a rx/0
